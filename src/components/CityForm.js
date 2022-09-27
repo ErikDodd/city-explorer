afb1a40a13205be366590189acd18da98a082850
ERIK M. DODD <emdodd27@gmail.com>
2022-09-26 17:33:54 -0700

    Map displaying and everything working

---
+++ src/components/CityForm.js
@@ -0,0 +1,38 @@
+import React from 'react';
+import Form from 'react-bootstrap/Form';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Button from 'react-bootstrap/Button';
+
+
+class CityForm extends React.Component {
+    render() {
+        return (
+            <>
+            <Form onSubmit={this.props.handleSearch}>
+                    <Form.Group className="mb-3" controlId="location">
+                        <Form.Label>Location</Form.Label>
+                        <Form.Control onChange={this.props.handleInput} type="text" placeholder="Search for a City" />
+                        <Form.Text className="text-muted">
+                            {/* {
+                                this.props.location.display_name &&
+                                <>
+                                    <h2>The City is {this.props.location.display_name}</h2>
+                                    <h2>The lat is {this.props.location.lat}</h2>
+                                    <h2>The lon is {this.props.location.lon}</h2>
+                                </>
+                            }
+                            {
+                                this.props.error &&
+                                <h2>Oh no! {this.props.errorMessage} </h2>
+                            } */}
+                        </Form.Text>
+                    </Form.Group>
+                    <Button onClick={this.props.handleSearch} variant="primary" type="submit">
+                        Submit
+                    </Button>
+            </Form>
+            </>
+        )
+    }
+}
+export default CityForm;
---
+++ src/components/Main.js
@@ -0,0 +1,69 @@
+import React from 'react';
+import axios from 'axios';
+import CityForm from './CityForm';
+import CityCard from './CityCard';
+
+
+class Main extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            searchQuery: '',
+            location: {},
+            error: false,
+            errorMessage: '',
+            cityMap: '',
+        }
+    }
+    handleInput = (e) => {
+        console.log(e.target);
+        // e.preventDefault();
+        this.setState({ searchQuery: e.target.value });
+        console.log(this.state.searchQuery);
+    }
+
+    // async await
+    handleSearch = async (e) => {
+        // code runs in the try block
+        e.preventDefault();
+        try {
+            const API = `https://us1.locationiq.com/v1/search.php?key=${process.env.REACT_APP_LOCATION_IQ_KEY}&q=${this.state.searchQuery}&format=json`
+            const res = await axios.get(API);
+            // you want res.data
+            console.log(res.data[0]); // this represents the actual Seattle about we want
+            this.setState({ 
+                location: res.data[0],  
+                cityMap: `https://maps.locationiq.com/v3/staticmap?key=${process.env.REACT_APP_LOCATION_IQ_KEY}&center=${res.data[0].lat},${res.data[0].lon}&zoom=12`,
+
+            });
+    
+        } catch (error) {
+            console.log(error);
+            this.setState({ error: true });
+            this.setState({ errorMessage: error.message });
+        }
+    }
+    render() {
+        return (
+           <>
+                <CityForm
+                    handleInput={this.handleInput}
+                    handleSearch={this.handleSearch}
+                    location={this.state.location}
+                    error={this.state.error}
+                    errorMessage={this.state.errorMessage}
+
+                />
+                <CityCard
+                    location={this.state.location}
+                    cityMap={this.state.cityMap}
+                />
+             
+           </> 
+        )
+
+    }
+}
+
+
+export default Main;
